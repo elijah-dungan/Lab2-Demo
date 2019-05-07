43542d8e3e3812d86083b0447b96abd4785fdaa6
root <eli.dungan@gmail.com>
2019-05-07 16:49:03 -0700

    added functioning randomized responses to guessing game

--- js/app.js
+++ js/app.js
@@ -5,18 +5,21 @@
 /* randomizes responses */
 var ding = ['Correct!', 'Great guess!', 'Yes!', 'Wow! You\'re good at this!'] // array of dings
 var zonk = ['Wrong!', 'Incorrect!', 'No!', 'A four year old can guess better!'] // array of zonks
-var randomDing = ding[Math.floor(Math.random()*ding.length)]; // selects randomly from dings
-var randomZonk = zonk[Math.floor(Math.random()*zonk.length)]; // selects randomly from zonks
+
+function rand(arrayInput) { 
+    var randomIndex = Math.floor((Math.random()*ding.length));
+    return (arrayInput[randomIndex]);
+}
 
 /* question One */
 var qOne = prompt('Is Elijah 5 feet, 9 inches tall?')  
 qOne = qOne.toLowerCase(); // normalizes strings to lowercase
 if(qOne === 'yes' || qOne === 'y') {
 console.log('Elijah is 5 feet, 9 inches tall.')
-alert(randomDing + ' Elijah is 5 feet, 9 inches tall.')
+alert(rand(ding) + ' Elijah is 5 feet, 9 inches tall.')
 } else {
 console.log('Elijah is 5 feet, 9 inches tall.')
-alert(randomZonk + ' Elijah is 5 feet, 9 inches tall.')
+alert(rand(zonk) + ' Elijah is 5 feet, 9 inches tall.')
 };
 
 /* question Two */
@@ -24,10 +27,10 @@ var qTwo = prompt('Does Elijah weigh 195 lbs?')
 qTwo = qTwo.toLowerCase(); // normalizes strings to lowercase
 if(qTwo === 'yes' || qTwo === 'y') { 
 console.log('Elijah weighs 175 lbs.')
-alert(randomZonk + ' Elijah weighs 175 lbs.')
+alert(rand(zonk) + ' Elijah weighs 175 lbs.')
 } else {
 console.log('Elijah weighs 175 lbs.')
-alert(randomDing + ' Elijah weighs 175 lbs.')
+alert(rand(ding) + ' Elijah weighs 175 lbs.')
 };
 
 /* question Three */
@@ -35,10 +38,10 @@ var qThree = prompt('Did Elijah serve in the military?')
 qThree = qThree.toLowerCase(); // normalizes strings to lowercase
 if(qThree === 'yes' || qThree === 'y') {
 console.log('Elijah served in the Army.')
-alert(randomDing + ' Elijah served in the Army.')
+alert(rand(ding) + ' Elijah served in the Army.')
 } else {
 console.log('Elijah served in the Army.')
-alert(randomZonk + ' Elijah served in the Army.')
+alert(rand(zonk) + ' Elijah served in the Army.')
 };
 
 /* question Four */
@@ -46,10 +49,10 @@ var qFour = prompt('Was Elijah born in Germany?')
 qFour = qFour.toLowerCase(); // normalizes strings to lowercase
 if(qFour === 'yes' || qFour === 'y') {
 console.log('Elijah was born in Germany.')
-alert(randomDing + ' Elijah was born in Germany')
+alert(rand(ding) + ' Elijah was born in Germany')
 } else {
 console.log('Elijah was born in Germany.')
-alert(randomZonk + ' Elijah was born in Germany.')
+alert(rand(zonk) + ' Elijah was born in Germany.')
 };
 
 /* question Five */
@@ -58,8 +61,8 @@ qFive = qFive.toLowerCase(); // normalizes strings to lowercase
 /* question Five if/else statement */
 if(qFive === 'yes' || qFive === 'y') {
 console.log('Elijah is Korean.')
-alert(randomDing + ' Elijah is Korean.')
+alert(rand(ding) + ' Elijah is Korean.')
 } else {
 console.log('Elijah is Korean.')
-alert(randomZonk + ' Elijah is Korean.')
+alert(rand(zonk) + ' Elijah is Korean.')
 };
