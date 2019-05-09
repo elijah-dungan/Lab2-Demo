cdc3756ae303ab03222e423bc0b6cbcdcfedac3f
root <eli.dungan@gmail.com>
2019-05-09 08:29:49 -0700

    added score tracker with message at the end

--- js/app.js
+++ js/app.js
@@ -5,26 +5,32 @@
 /* wraps script in function */
 function guessingGame() {
 
-    /* randomizes initial responses. number of dings and zonks must be the same, or alert may return undefined */
+    /* Array of initial responses. Number of dings and zonks must be the same, or alert may return undefined */
     var ding = ['Correct!', 'Great guess!', 'That is true!', 'That is correct!', 'Wow! You\'re good at this!', 'Ding! Ding! Ding! We have a winner!', 'Did you peak at the code?']; // array of dings
+    var zonk = ['Wrong!', 'Incorrect!', 'That is false!', 'That is incorrect!', 'A four year old can guess better!', 'Zooooooonk!', 'Don\'t quit your day job.']; //array of zonks
+    
+    /* array that keeps track of scores */
+    var scoreArray = [];
+
     var dLength = ding.length; // gets total number of strings in array
-    var zonk = ['Wrong!', 'Incorrect!', 'That is false!', 'That is incorrect!', 'A four year old can guess better!', 'Zooooooonk!', 'Don\'t quit your day job.']; 
     var zLength = zonk.length; // gets total number of strings in array
-    /* selects random variable */
-    function rand(arrayInput) { 
-        var randomIndex = Math.floor((Math.random()*(dLength + zLength)/2)); 
-        return (arrayInput[randomIndex]);
+
+    /* Randomizes dings and zonks based on total number of dings and zonks divided by 2*/
+    function randomIndex(array) { 
+        var i = Math.floor((Math.random()*(dLength + zLength)/2)); 
+        return (array[i]);
     }
 
     /* question One */
     var qOne = prompt('Is Elijah 5 feet, 9 inches tall?');  
     qOne = qOne.toLocaleLowerCase(); // normalizes strings to lowercase
     if(qOne === 'yes' || qOne === 'y') {
+        scoreArray.push(1); // adds new value to array for score tracking
         console.log('Elijah is 5 feet, 9 inches tall.');
-        alert(rand(ding) + ' Elijah is 5 feet, 9 inches tall.');
+        alert(randomIndex(ding) + ' Elijah is 5 feet, 9 inches tall.');
     } else if(qOne === 'no' || qOne === 'n') {
         console.log('Elijah is 5 feet, 9 inches tall.');
-        alert(rand(zonk) + ' Elijah is 5 feet, 9 inches tall.');
+        alert(randomIndex(zonk) + ' Elijah is 5 feet, 9 inches tall.');
     } else {
         console.log('Elijah is 5 feet, 9 inches tall.');
         alert('Elijah is 5 feet, 9 inches tall.'); 
@@ -35,10 +41,11 @@ function guessingGame() {
     qTwo = qTwo.toLocaleLowerCase(); // normalizes strings to lowercase
     if(qTwo === 'yes' || qTwo === 'y') { 
         console.log('Elijah weighs 175 lbs, not 195 lbs.');
-        alert(rand(zonk) + ' Elijah weighs 175 lbs, not 195 lbs.');
+        alert(randomIndex(zonk) + ' Elijah weighs 175 lbs, not 195 lbs.');
     } else if(qTwo === 'no' || qTwo === 'n') {
+        scoreArray.push(2); // adds new value to array for score tracking
         console.log('Elijah weighs 175 lbs, not 195 lbs.');
-        alert(rand(ding) + ' Elijah weighs 175 lbs, not 195 lbs.');
+        alert(randomIndex(ding) + ' Elijah weighs 175 lbs, not 195 lbs.');
     } else {
         console.log('Elijah weighs 175 lbs, not 195 lbs.');
         alert('Elijah weighs 175 lbs, not 195 lbs.');
@@ -48,11 +55,12 @@ function guessingGame() {
     var qThree = prompt('Did Elijah serve in the military?');
     qThree = qThree.toLocaleLowerCase(); // normalizes strings to lowercase
     if(qThree === 'yes' || qThree === 'y') {
+        scoreArray.push(3); // adds new value to array for score tracking
         console.log('Elijah served in the Army.');
-        alert(rand(ding) + ' Elijah served in the Army.');
+        alert(randomIndex(ding) + ' Elijah served in the Army.');
     } else if(qThree === 'no' || qThree === 'n') {
         console.log('Elijah served in the Army.');
-        alert(rand(zonk) + ' Elijah served in the Army.');
+        alert(randomIndex(zonk) + ' Elijah served in the Army.');
     } else {
         console.log('Elijah served in the Army.');
         alert('Elijah served in the Army.');
@@ -62,11 +70,12 @@ function guessingGame() {
     var qFour = prompt('Was Elijah born in Germany?'); 
     qFour = qFour.toLocaleLowerCase(); // normalizes strings to lowercase
     if(qFour === 'yes' || qFour === 'y') {
+        scoreArray.push(4); // adds new value to array for score tracking
         console.log('Elijah was born in Germany.');
-        alert(rand(ding) + ' Elijah was born in Germany');
+        alert(randomIndex(ding) + ' Elijah was born in Germany.');
     } else if(qFour === 'no' || qFour === 'n') {
         console.log('Elijah was born in Germany.');
-        alert(rand(zonk) + ' Elijah was born in Germany.');
+        alert(randomIndex(zonk) + ' Elijah was born in Germany.');
     } else {
         console.log('Elijah was born in Germany.');
         alert('Elijah was born in Germany.');
@@ -76,22 +85,38 @@ function guessingGame() {
     var qFive = prompt('Is Elijah Korean?'); 
     qFive = qFive.toLocaleLowerCase(); // normalizes strings to lowercase
     if(qFive === 'yes' || qFive === 'y') {
+        scoreArray.push(5); // adds new value to array for score tracking
         console.log('Elijah is Korean.');
-        alert(rand(ding) + ' Elijah is Korean.');
+        alert(randomIndex(ding) + ' Elijah is Korean.');
     } else if(qFive === 'no' || qFive === 'n') {
         console.log('Elijah is Korean.');
-        alert(rand(zonk) + ' Elijah is Korean.');
+        alert(randomIndex(zonk) + ' Elijah is Korean.');
     } else {
         console.log('Elijah is Korean.');
         alert('Elijah is Korean.');
     }
 
-    /* question Six */
+    /* for loop that calculates total score */
+    for(var i = 0; i <scoreArray.length; i++) {
+        var totalScore = i + 1;
+    }
+
+    /* gives alert based on total score, must update values when adding questions! */
+    if(totalScore === 5) {
+        alert('Congratulations! You scored ' + totalScore + ' points! You guessed right on all the questions!');
+    } else if(totalScore >= 3) {
+        alert('Not bad! You scored ' + totalScore + ' points! You guessed right on more than half the questions!');
+    } else if(totalScore < 3) {
+        alert('Wow that was bad! You scored ' + totalScore + ' points and guessed right on less than half of the questions.');
+    } 
+
+    /* additional questions
     var qSix = prompt('Has Elijah met someone famous?'); 
     qSix = qSix.toLocaleLowerCase(); // normalizes strings to lowercase
     if(qSix === 'yes' || qSix === 'y') {
         console.log('Elijah has met two sports hall-of-fame inductees.');
         alert(rand(ding) + ' Elijah has met two sports hall-of-fame inductees.');
+        scoreArray.push(1);
     } else if(qSix === 'no' || qSix === 'n') {
         console.log('Elijah has met two sports hall-of-fame inductees.');
         alert(rand(zonk) + ' Elijah has met two sports hall-of-fame inductees.');
@@ -100,12 +125,12 @@ function guessingGame() {
         alert('Elijah has met two sports hall-of-fame inductees.');
     }
 
-    /* question Seven */
     var qSeven = prompt('Does Elijah ride a motorcycle?'); 
     qSeven = qSeven.toLocaleLowerCase(); // normalizes strings to lowercase
     if(qSeven === 'yes' || qSeven === 'y') {
         console.log('Elijah rides a motorcyle.');
         alert(rand(ding) + ' Elijah rides a motorcyle.');
+        scoreArray.push(1);
     } else if(qSeven === 'no' || qSeven === 'n') {
         console.log('Elijah rides a motorcyle.');
         alert(rand(zonk) + ' Elijah rides a motorcyle.');
@@ -114,7 +139,6 @@ function guessingGame() {
         alert('Elijah rides a motorcyle.');
     }
 
-    /* question Eight */
     var qEight = prompt('Does Elijah have kids?'); 
     qEight = qEight.toLocaleLowerCase(); // normalizes strings to lowercase
     if(qEight === 'yes' || qEight === 'y') {
@@ -123,9 +147,10 @@ function guessingGame() {
     } else if(qEight === 'no' || qEight === 'n') {
         console.log('Elijah does not have kids.');
         alert(rand(ding) + ' Elijah does not have kids.');
+        scoreArray.push(1);
     } else {
         console.log('Elijah does not have kids.');
         alert('Elijah does not have kids.');
-    }
+    } */
 
 };
